e29c01275a7aea45645f0bb80bbd5c65d75d9411
sohamohsen <sohamohsen016@gmail.com>
2025-12-16 02:57:16 +0200

    first commit

---
+++ src/main/java/com/soha/food/foodiesapi/FoodieApplication.java
@@ -0,0 +1,14 @@
+package com.soha.food.foodiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieApplication.class, args);
+    }
+
+}
